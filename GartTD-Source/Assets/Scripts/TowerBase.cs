97bde038976b9a1a1cff35942137571232a527cc
JulianKirk <progamerguy005@gmail.com>
2022-08-16 08:50:01 +1000

    Created some of the tower base class

--- GartTD-Source/Assets/Scripts/TowerBase.cs
+++ GartTD-Source/Assets/Scripts/TowerBase.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public abstract class TowerBase
 {
-	public abstract enum targetingMode {First, Last, Strongest};
-	public abstract enum attackType {Physical, Incendiary, Electric};
+	protected enum targetingMode {First, Last, Strongest};
+
+	protected enum attackType {Physical, Incendiary, Electric};
+
 	public abstract targetingMode currentTargetingMode;
+
+	public abstract attackType curentAttackType;
+
 	public abstract float attackRange;
+
+	public abstract float attackSpeed;
 
 }
